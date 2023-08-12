07705c43699cbf91e751cd93b1ade07f3c31ee44
Shadman Khan <shaad2209@gmail.com>
2023-08-12 19:56:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,49 @@ import Navbar from './components/Navbar';
 import Slider from './components/Slider';
 import data from "./data/data.json";
 import Banner from "./components/Banner.jsx";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Offers from './components/Offers';
+import Heading from './components/Heading';
+import StarProduct from './components/StarProduct';
+import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.jsx"
+import HotAccessories from "./components/HotAccessories.jsx"
 
 function App() {
   return (
     <div className="App">
+      <Router>
     <PreNavbar />
     <Navbar />
     <Slider  start={data.banner.start} />
+    <Offers  offer={data.offer} />
+    <Heading text="STAR PRODUCTS"  />
+    <StarProduct  starProduct={data.starProduct} />
+    <Heading text="HOT ACCESSORIES"  />
+    <HotAccessoriesMenu  />
+
+    <Route exact path="/music">
+       <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />
+       </Route>
+
+       <Route exact path="/smartDevice">
+       <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />
+       </Route>
+
+       <Route exact path="/home">
+       <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />
+       </Route>
+
+       <Route exact path="/lifestyle">
+       <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />
+       </Route>
+
+
+       <Route exact path="/mobileAccessories">
+       <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />
+       </Route>
+
+
+    </Router>
     </div>
   );
 }
